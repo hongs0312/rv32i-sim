84a4ff641b1947ee580cbe5557c3b1636037a7dc
hongs0312 <hongseongho300@gmail.com>
2026-08-04 21:44:24 +0900

    주석 추가

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -9,29 +9,27 @@ impl Dram {
         }
     }
 
+    // Load methods
     pub fn load8(&self, addr: usize) -> u8 {
         u8::from_le_bytes([self.dram[addr]])
     }
-
     pub fn load16(&self, addr: usize) -> u16 {
         let bytes = &self.dram[addr..addr + 2];
         u16::from_le_bytes(bytes.try_into().expect("Slice with incorrect length"))
     }
-
     pub fn load32(&self, addr: usize) -> u32 {
         let bytes = &self.dram[addr..addr + 4];
         u32::from_le_bytes(bytes.try_into().expect("Slice with incorrect length"))
     }
 
+    // Store methods
     pub fn store8(&mut self, addr: usize, value: u8) {
         self.dram[addr] = value;
     }
-
     pub fn store16(&mut self, addr: usize, value: u16) {
         let bytes = value.to_le_bytes();
         self.dram[addr..addr + 2].copy_from_slice(&bytes);
     }
-
     pub fn store32(&mut self, addr: usize, value: u32) {
         let bytes = value.to_le_bytes();
         self.dram[addr..addr + 4].copy_from_slice(&bytes);
